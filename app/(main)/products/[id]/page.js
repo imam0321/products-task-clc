d4bacfe6d6efb66e93de2c78b27ceb83f54ed455
imam0321 <imam.hossain0321@gmail.com>
2025-04-25 15:00:01 +0600

    feat: enable dynamic rendering and revalidation for ProductDetailsPage

--- app/(main)/products/[id]/page.js
+++ app/(main)/products/[id]/page.js
@@ -1,6 +1,8 @@
 import { getProductById } from "@/app/actions/products";
 import ProductDetails from "@/components/ProductDetails/ProductDetails";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 60; 
 
 export default async function ProductDetailsPage({ params: { id } }) {
   const product = await getProductById(id);
