47d508ea5076739a10c0e92f5c77eadc8eec7b4f
imam0321 <imam.hossain0321@gmail.com>
2025-04-26 19:08:52 +0600

    fix: text

--- components/CartDetails/CartDetails.jsx
+++ components/CartDetails/CartDetails.jsx
@@ -9,7 +9,7 @@ export default function CartDetails() {
   return (
     <div className="fixed bg-[#12141D] right-0 lg:w-[700px] mid:w-full w-full shadow-2xl lg:mt-[4.4rem] mt-[4.8rem] pb-10 z-20 transition-all duration-500">
       <div className="md:p-8 p-5">
-        <h2 className={`lg:text-3xl text-xl font-bold text-yellow-500 mb-6 sm:mb-8 ${!products.length > 0 && "text-white me-10"}`}>
+        <h2 className={`lg:text-3xl text-xl text-white  font-bold mb-6 sm:mb-8 ${!products.length > 0 && "me-10"}`}>
           Your Carts
         </h2>
         <div className="max-h-[400px] overflow-y-auto px-4 z-30">
